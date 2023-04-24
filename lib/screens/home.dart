4dba90a831998ce20f2ba945de8f613b2e6e91f3
MarianneSyntax <113111255+MarianneSyntax@users.noreply.github.com>
2023-04-24 16:35:58 +0100

    added visibility detector to update home screen when changes are made

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -3,6 +3,7 @@ import 'package:forma_flutter/local_data/DatabaseHelper.dart';
 import 'package:forma_flutter/model/habit.dart';
 import 'package:forma_flutter/widgets/habit_row.dart';
 import 'package:forma_flutter/screens/detail.dart';
+import 'package:visibility_detector/visibility_detector.dart';
 
 class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
@@ -23,57 +24,67 @@ class _HomeState extends State<Home> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          title: const Text(
-            'My Habits',
-            style: TextStyle(fontSize: 26, fontWeight: FontWeight.bold),
+    return VisibilityDetector(
+      key: Key('home'),
+      onVisibilityChanged: (visibilityInfo) {
+        if (visibilityInfo.visibleFraction == 1) {
+          setState(() {
+            future = DatabaseHelper.instance.getHabits();
+          });
+        }
+      },
+      child: Scaffold(
+          appBar: AppBar(
+            title: const Text(
+              'My Habits',
+              style: TextStyle(fontSize: 26, fontWeight: FontWeight.bold),
+            ),
           ),
-        ),
-        body: Column(
-          children: [
-            FutureBuilder<List<Habit>>(
-                future: future,
-                builder: (BuildContext context,
-                    AsyncSnapshot<List<Habit>> snapshot) {
-                  if (!snapshot.hasData) {
-                    return const Center(child: CircularProgressIndicator());
-                  }
-                  return snapshot.data!.isEmpty
-                      ? const Center(child: Text("No habits yet. Add one!"))
-                      : ListView(
-                          shrinkWrap: true,
-                          children: snapshot.data!
-                              .map((habit) => Dismissible(
-                                  key: Key(habit.id.toString()),
-                                  background: Container(
-                                    color: Colors.red,
-                                    child: const Icon(Icons.delete),
-                                  ),
-                                  direction: DismissDirection.endToStart,
-                                  onDismissed: (direction) {
-                                    DatabaseHelper.instance
-                                        .deleteHabit(habit.id!);
-                                    setState(() {
-                                      future =
-                                          DatabaseHelper.instance.getHabits();
-                                      snapshot.data!.remove(habit);
-                                    });
-                                  },
-                                  child: HabitRow(habit: habit)))
-                              .toList(),
-                        );
-                }),
-          ],
-        ),
-        floatingActionButton: FloatingActionButton(
-            onPressed: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => Detail()),
-              );
-            },
-            child: const Icon(Icons.add),
-            backgroundColor: Colors.purple));
+          body: Column(
+            children: [
+              FutureBuilder<List<Habit>>(
+                  future: future,
+                  builder: (BuildContext context,
+                      AsyncSnapshot<List<Habit>> snapshot) {
+                    if (!snapshot.hasData) {
+                      return const Center(child: CircularProgressIndicator());
+                    }
+                    return snapshot.data!.isEmpty
+                        ? const Center(child: Text("No habits yet. Add one!"))
+                        : ListView(
+                            shrinkWrap: true,
+                            children: snapshot.data!
+                                .map((habit) => Dismissible(
+                                    key: Key(habit.id.toString()),
+                                    background: Container(
+                                      color: Colors.red,
+                                      child: const Icon(Icons.delete),
+                                    ),
+                                    direction: DismissDirection.endToStart,
+                                    onDismissed: (direction) {
+                                      DatabaseHelper.instance
+                                          .deleteHabit(habit.id!);
+                                      setState(() {
+                                        future =
+                                            DatabaseHelper.instance.getHabits();
+                                        snapshot.data!.remove(habit);
+                                      });
+                                    },
+                                    child: HabitRow(habit: habit)))
+                                .toList(),
+                          );
+                  }),
+            ],
+          ),
+          floatingActionButton: FloatingActionButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => Detail()),
+                );
+              },
+              child: const Icon(Icons.add),
+              backgroundColor: Colors.purple)),
+    );
   }
 }
